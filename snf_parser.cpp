36da4083c5f51df4efef85d1a3971afb9e98f39e
olerapx <olerapx0@gmail.com>
2015-04-13 17:02:59 +0300

    fix issues. again

--- snf_parser.cpp
+++ snf_parser.cpp
@@ -8,9 +8,9 @@ FunctionType SNF_Parser::parse(std::string input, Expression &output)
      removeUnused();
    if (!checkBrackets()) return OTHER;
    if (!checkOperationAfterInverse()) return OTHER;
-     std::vector <std::string> variables;
-  ft= getVariables(variables);
-
+   std::vector <std::string> variables;
+   ft= getVariables(variables);
+  //  for (int i=0;i<variables.size();i++) std::cout<< variables[i]<<" ";
     if (isVariablesRepeat(variables)) return OTHER;
 
     switch(ft)
@@ -142,17 +142,17 @@ std::string SNF_Parser::getOperand (int &index)
            getSymbolType(_input[index])!=SYMBOL_RBRACKET &&
                  getSymbolType(_input[index])!=SYMBOL_ZERO)
     {
-         if (getSymbolType(_input[index])==SYMBOL_INVERSE)
+         if (getSymbolType(_input[index])==SYMBOL_INVERSE && index>0 &&
+                 (getSymbolType(_input[index-1])==SYMBOL_OPERAND ||getSymbolType(_input[index-1])==SYMBOL_RBRACKET ))
          {
-          _input.insert(index, "&");
-          index+=2;
-          return output;
+              _input.insert(index, "&");
+              index+=1;
+              return output;
          }
        else if (getSymbolType(_input[index])==SYMBOL_OPERAND) output+=_input[index];
         index++;
     }
     index++;
-
    return output;
    }
 
@@ -160,8 +160,9 @@ int SNF_Parser::checkOperandAfterLBracket(int index, int isDisjunctionCurr)
 {
        getOperand(index);
        if (getSymbolType(_input[index-1])==SYMBOL_DISJUNCTION){
-           if (isDisjunctionCurr==-1) return -2;
-           else if (isDisjunctionCurr==0) return -1;
+
+           if (isDisjunctionCurr==0) return -1;
+         else if (isDisjunctionCurr==-1) return -2;
                else isDisjunctionCurr=1;
        }
        else if (getSymbolType(_input[index-1])==SYMBOL_CONJUNCTION){
@@ -192,7 +193,7 @@ bool SNF_Parser::checkOperationAfterInverse()
 {
  int len=_input.size();
  for (int i=0;i<len-1;i++)
-     if (getSymbolType(_input[i])== SYMBOL_INVERSE && getSymbolType(_input[i])!=SYMBOL_OPERAND) return 0;
+     if (getSymbolType(_input[i])== SYMBOL_INVERSE && getSymbolType(_input[i+1])!=SYMBOL_OPERAND) return 0;
 
  if (getSymbolType(_input[len-1])==SYMBOL_INVERSE) return 0;
  return 1;
