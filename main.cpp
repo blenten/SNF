56a6d939a563c167becb6fc858a04cd0bc64a0b7
olerapx <olerapx0@gmail.com>
2015-05-30 15:49:32 +0300

    Merge branch 'dev' of https://github.com/blenten/SNF into dev
    Conflicts:
    	main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 void expressionOutputSample(const Expression&  ex, const FunctionType& ft);
 
-int main()
+int main(int argc, char* argv[])
 {
 
     QApplication app(argc, argv);
@@ -26,55 +26,4 @@ int main()
     //cin.clear();
     //cin.get();
     return app.exec();
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-///this was in main.cpp
-/*  SNF_Minimizer snf;
-  string result;
-  //result = snf.minimize("(x1+x2+x3)(!x1+x2+x3)(!x1+!x2+x3)(!x1+!x2+!x3)"); ///commented MUHAHAHAHAHA
-  result=snf.minimize("x1!!!!x2+ !x1*x2");
-  cout<<"RES: "<<result<<endl;
-*/
-
-
-///You really don't need to comment this
-// YAP :p
-
-///motherfucker
-/*
-void expressionOutputSample(const Expression&  ex, const FunctionType &ft)
-{
-
-    switch(ft)
-    {
-
-        case OTHER: std::cout <<"OTHER\n"; break;
-        case SNDF: std::cout <<"SNDF\n"; break;
-        case SNKF: std::cout << "SNKF\n"; break;
-    }
-
-    for (int i=0;i<ex.size();i++)
-    {
-        for (int j=0;j<ex[i].size();j++)
-            if (ex[i][j].invertion)
-            std::cout <<"!"<<ex[i][j].name<<" ";
-          else std::cout<<ex[i][j].name<<" ";
-        std::cout <<"\n";
-    }
-}
-
-*/
